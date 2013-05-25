4d49df5b8f895bf75d56688efeb97cf265cfe266
Agaricus <agaricusb@yahoo.com>
2013-05-24 21:46:58 -0700

    Use same raytracing method as buckets, to pickup liquids

--- src/main/java/agaricus/mods/highlighttips/HighlightTips.java
+++ src/main/java/agaricus/mods/highlighttips/HighlightTips.java
@@ -126,6 +126,30 @@ public class HighlightTips implements ITickHandler {
         return sb.toString();
     }
 
+    // copied from net/minecraft/item/Item since it is needlessly protected
+    protected MovingObjectPosition getMovingObjectPositionFromPlayer(World par1World, EntityPlayer par2EntityPlayer, boolean isBucketEmpty)
+    {
+        float f = 1.0F;
+        float f1 = par2EntityPlayer.prevRotationPitch + (par2EntityPlayer.rotationPitch - par2EntityPlayer.prevRotationPitch) * f;
+        float f2 = par2EntityPlayer.prevRotationYaw + (par2EntityPlayer.rotationYaw - par2EntityPlayer.prevRotationYaw) * f;
+        double d0 = par2EntityPlayer.prevPosX + (par2EntityPlayer.posX - par2EntityPlayer.prevPosX) * (double)f;
+        double d1 = par2EntityPlayer.prevPosY + (par2EntityPlayer.posY - par2EntityPlayer.prevPosY) * (double)f + 1.62D - (double)par2EntityPlayer.yOffset;
+        double d2 = par2EntityPlayer.prevPosZ + (par2EntityPlayer.posZ - par2EntityPlayer.prevPosZ) * (double)f;
+        Vec3 vec3 = par1World.getWorldVec3Pool().getVecFromPool(d0, d1, d2);
+        float f3 = MathHelper.cos(-f2 * 0.017453292F - (float) Math.PI);
+        float f4 = MathHelper.sin(-f2 * 0.017453292F - (float)Math.PI);
+        float f5 = -MathHelper.cos(-f1 * 0.017453292F);
+        float f6 = MathHelper.sin(-f1 * 0.017453292F);
+        float f7 = f4 * f5;
+        float f8 = f3 * f5;
+        double d3 = 5.0D;
+        if (par2EntityPlayer instanceof EntityPlayerMP)
+        {
+            d3 = ((EntityPlayerMP)par2EntityPlayer).theItemInWorldManager.getBlockReachDistance();
+        }
+        Vec3 vec31 = vec3.addVector((double)f7 * d3, (double)f6 * d3, (double)f8 * d3);
+        return par1World.rayTraceBlocks_do_do(vec3, vec31, isBucketEmpty, !isBucketEmpty);
+    }
 
     @Override
     public void tickEnd(EnumSet<TickType> type, Object... tickData) {
@@ -136,7 +160,7 @@ public class HighlightTips implements ITickHandler {
         if (screen != null) return;
 
         float partialTickTime = 1;
-        MovingObjectPosition mop = mc.thePlayer.rayTrace(range, partialTickTime);
+        MovingObjectPosition mop = getMovingObjectPositionFromPlayer(mc.theWorld, mc.thePlayer, true);
         String s;
 
         if (mop == null) {
